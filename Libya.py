d59a9a6a58381974836122c94fee87bed8dda1fb
mhmdalrmram92 <61891647+mhmdalrmram92@users.noreply.github.com>
2020-04-11 17:25:50 +0200

    Create Libya.py

---
+++ Libya.py
@@ -0,0 +1,7 @@
+استيراد [عدل] os
+(نظام التشغيل "clear"
+طباعة [عدل] ("ليبيا")
+[عدل]
+https://github.com/UndeadSec/SocialFish
+https://github.com/thelinuxchoice/shellphish
+https://github.com/DarkSecDevelopers/HiddenEye
